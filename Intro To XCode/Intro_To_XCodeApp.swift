bd3983dd8c2ecf1ccb10ee433ad5a36160f2e08d
Julie Hum <julieehum@gmail.com>
2023-08-10 16:15:35 -0400

    Initial Commit

---
+++ Intro To XCode/Intro_To_XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_To_XCodeApp.swift
+//  Intro To XCode
+//
+//  Created by Julie E. Hum on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Intro_To_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
